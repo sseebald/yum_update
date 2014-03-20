459632ae9f6d8de1a32dedff6e67f5384ee7b3b4
Spencer Seebald <sseebald@gmail.com>
2014-03-20 00:16:23 +0000

    Cleaned up code, added in support for systems using apt. Currently working on Debian and Red Hat based systems - I think Windows will be out of scope for this project.

--- files/yum_update.sh
+++ files/yum_update.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-if [[ -n $(yum -e0 -d0 check-update) ]];
+declare -x FACTER="/opt/puppet/bin/facter --no-external-dir"
+
+if [[ $($FACTER osfamily) == "RedHat" ]];
 then
-	string="Yes"
+	if [[ -n $(yum -e0 -d0 check-update) ]];
+	then
+		string="Yes"
+	else
+		string="No"
+	fi
+elif [[ $($FACTER osfamily) == "Debian" ]];
+then
+	if [[ -n $(aptitude search ~U | grep -v "Generic Linux") ]];
+	then
+		string="Yes"
+	else
+		string="No"
+	fi
 else
-	string="No"
+	string="OS not supported"
 fi
 
 echo "Has_Updates=${string}"
